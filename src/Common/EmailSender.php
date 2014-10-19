7405b25db5344dee4764b643fc23b92e30a959e6
Tomas Voslar <tomas.voslar@webcook.cz>
2014-10-19 15:16:50 +0100

    Removed attachment from email

--- src/Common/EmailSender.php
+++ src/Common/EmailSender.php
@@ -46,12 +46,12 @@ class EmailSender
 	    $name = ucfirst(\Nette\Utils\Strings::webalize($this->investment->getAddress()->getName()));
 	    $lastname = ucfirst(\Nette\Utils\Strings::webalize($this->investment->getAddress()->getLastName()));
 
-	    $filePath = $this->type == 'form' ? 
+	   /* $filePath = $this->type == 'form' ? 
 	    'zajistena-investice_Kalkulace_' . $name . '-' . $lastname . '.pdf' :
     	'zajistena-investice_Smlouva_' . $name . '-' . $lastname . '.pdf';
 		file_put_contents($filePath, $emailAttachment);
 
-		$mail->addAttachment($filePath, null, 'application/pdf');
+		$mail->addAttachment($filePath, null, 'application/pdf');*/
 
 		$mail->send();
 
