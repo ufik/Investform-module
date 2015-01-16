9941f52e282efe0141bfb415e8bd17cbba3827bd
Jakub Šanda <sanda.jakubhtc@gmail.com>
2015-01-16 13:54:01 +0100

    businessman entity and adding

--- src/Admin/presenters/BusinessmanPresenter.php
+++ src/Admin/presenters/BusinessmanPresenter.php
@@ -17,7 +17,7 @@ use Nette\Forms\Form;
 class BusinessmanPresenter extends BasePresenter
 {
     
-
+    private $businessman;
 
     protected function startup()
     {
@@ -39,4 +39,66 @@ class BusinessmanPresenter extends BasePresenter
     	$this->reloadContent();
     	$this->template->idPage = $idPage;
     }
+
+    public function actionUpdate($id, $idPage)
+    {
+        $this->reloadContent();
+        $this->template->idPage = $idPage;
+    }
+
+    public function createComponentForm($name)
+    {
+        $form = $this->createForm('form-submit', 'default', null);
+
+        $form->addText('name', 'Name')->setRequired('Name is mandatory.');
+        $form->addText('lastname', 'Lastname')->setRequired('Lastname is mandatory.');
+        $form->addText('street', 'Street and number')->setRequired('Street and number is mandatory.');
+        $form->addText('zipCity', 'Zip and city')->setRequired('Zip and city is mandatory.');
+        $form->addText('email', 'Email')
+            ->addRule(Form::EMAIL, 'This email is not valid.')
+            ->setRequired('Email is mandatory.');
+        $form->addText('phone', 'Phone')->setRequired('Phone is mandatory.');
+
+        //generated values
+        $form->addText('businessId', 'Generated businessman ID')
+            ->setValue(mt_rand(10000,99999))
+            ->setDisabled();
+
+        $form->addText('businessUrl', 'Generated businessman URL')
+            ->setValue(bin2hex(mcrypt_create_iv(10, MCRYPT_DEV_URANDOM)))
+            ->setDisabled();
+
+        $form->addSubmit('save', 'Save new businessman');
+
+        $form->onSuccess[] = callback($this, 'formSubmitted');
+
+        return $form;
+    }
+
+    public function formSubmitted($form)
+    {
+        $values = $form->getValues();
+
+        $this->businessman = new Businessman;
+        $this->businessman->setName($values->name);
+        $this->businessman->setLastname($values->lastname);
+        $this->businessman->setStreet($values->street);
+        $this->businessman->setZipCity($values->zipCity);
+        $this->businessman->setEmail($values->email);
+        $this->businessman->setTelephone($values->telephone);
+
+        $this->businessman->setBusinessId($values->businessId);
+        $this->businessman->setBusinessUrl($values->businessUrl);
+
+        $this->businessman->setActive(true);
+
+        $this->em->flush();
+
+        $this->flashMessage('Businessman has been updated.', 'success');
+        $this->forward('default', array(
+            'idPage' => $this->actualPage->getId()
+        ));
+    }
+
+    
 }
