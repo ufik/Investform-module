42abcb8f05650638fdaa6dbad89f8f73b862657f
Jakub Šanda <sanda.jakubhtc@gmail.com>
2015-01-16 12:45:54 +0100

    businessman presenters

---
+++ src/Admin/presenters/BusinessmanPresenter.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This file is part of the Investform module for webcms2.
+ * Copyright (c) @see LICENSE
+ */
+
+namespace AdminModule\InvestformModule;
+
+use Nette\Forms\Form;
+
+/**
+ * Description of
+ *
+ * @author Jakub Sanda <jakub.sanda@webcook.cz>
+ */
+class BusinessmanPresenter extends BasePresenter
+{
+    
+
+
+    protected function startup()
+    {
+    	parent::startup();
+    }
+
+    protected function beforeRender()
+    {
+	   parent::beforeRender();
+    }
+
+    public function actionDefault($idPage)
+    {
+
+    }
+
+    public function renderDefault($idPage)
+    {
+    	$this->reloadContent();
+    	$this->template->idPage = $idPage;
+    }
+}
---
+++ src/Frontend/presenters/BusinessmanPresenter.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This file is part of the Investform module for webcms2.
+ * Copyright (c) @see LICENSE
+ */
+
+namespace FrontendModule\InvestformModule;
+
+
+/**
+ * Description of InvestformPresenter
+ *
+ * @author Jakub Sanda <jakub.sanda@webcook.cz>
+ */
+class BusinessmanPresenter extends BasePresenter
+{
+	private $id;
+	
+	protected function startup() 
+    {
+		parent::startup();
+	}
+
+	protected function beforeRender()
+    {
+		parent::beforeRender();	
+	}
+
+	public function renderDefault($id)
+    {	
+		$this->template->id = $id;
+	}
+}
