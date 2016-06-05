2ec6cf164153c45a90fc6a87d4c5ab9f4f2a1a55
sandiCzech <sanda.jakubhtc@gmail.com>
2016-06-05 10:27:28 +0200

    fix contract calculation

--- src/Frontend/presenters/InvestformPresenter.php
+++ src/Frontend/presenters/InvestformPresenter.php
@@ -397,7 +397,7 @@ class InvestformPresenter extends BasePresenter
 		if ($length == 5) {
 			$to = strtotime('2020-11-30');
 		} else {
-			$to = strtotime('2019-11-30');
+			$to = strtotime('2017-10-30');
 		}
 
 		$length = ($to - $from) / 60 / 60 / 24 / 365;
